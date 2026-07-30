851bf22f6ae9426c683fa89797d83f4f50d9d7c9
micky <mickydl@freenet.de>
2026-07-30 19:36:17 +0200

    auto deploy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from block_markdown import markdown_to_html_node
@@ -7,17 +8,17 @@ from inline_markdown import extract_title
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 
 in_file = "index.md"
 out_file = "index.html"
-paths = [("./content","./public"), ("./content/blog/glorfindel", "./public/blog/glorfindel"), ("./content/blog/tom", "./public/blog/tom"), ("./content/blog/majesty","./public/blog/majesty"), ("./content/contact","./public/contact")]
+paths = [("./content","./docs"), ("./content/blog/glorfindel", "./docs/blog/glorfindel"), ("./content/blog/tom", "./docs/blog/tom"), ("./content/blog/majesty","./docs/blog/majesty"), ("./content/contact","./docs/contact")]
 template_path = "./template.html"
 
 #fp = "content/index.md"
 #dt = "public/index.html"
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str) -> None:
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
     markdown_file = open(from_path, "r")
     markdown = markdown_file.read()
@@ -28,6 +29,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     content = markdown_to_html_node(markdown)
     title = extract_title(markdown)
     html_page = template.replace("{{ Title }}",title).replace("{{ Content }}", content.to_html())
+    html_page = html_page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     if dest_path != "":
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     dst_file = open(dest_path, "w")
@@ -36,6 +38,12 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     
 
 def main() -> None:
+    basepath: str = "/"
+
+    print(f"-D- num args: {len(sys.argv)}, args: {sys.argv}")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -46,7 +54,7 @@ def main() -> None:
     for input_file_dir, output_file_dir in paths:
         input_file = os.path.join(input_file_dir,in_file)
         output_file = os.path.join(output_file_dir,out_file)
-        generate_page(input_file, template_path, output_file)
+        generate_page(input_file, template_path, output_file, basepath)
 
 main()
 
